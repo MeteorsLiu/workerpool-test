30546aa8e2f0c0f7778f46bb37b9bfcd6f2fab3a
GuoHaoLan <coopticks@hotmail.com>
2022-11-24 23:18:28 +0800

    a

--- worker_test.go
+++ worker_test.go
@@ -7,10 +7,30 @@ import (
 
 func TestWorker(t *testing.T) {
 	w := NewPool(8, 1024, 8)
-	for i := 0; i < 8; i++ {
-		w.Schedule(func() {
-			t.Log(i)
-		})
-	}
+
+	w.Schedule(func() {
+		t.Log(1)
+	})
+	w.Schedule(func() {
+		t.Log(2)
+	})
+	w.Schedule(func() {
+		t.Log(3)
+	})
+	w.Schedule(func() {
+		t.Log(4)
+	})
+	w.Schedule(func() {
+		t.Log(5)
+	})
+	w.Schedule(func() {
+		t.Log(6)
+	})
+	w.Schedule(func() {
+		t.Log(7)
+	})
+	w.Schedule(func() {
+		t.Log(8)
+	})
 	<-time.After(time.Minute)
 }
